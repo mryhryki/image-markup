b939ea724d436501dee933d82713d78231eb54a3
mryhryki <mryhryki@gmail.com>
2021-08-27 18:12:37 +0900

    Add DnD area, header and footer.

--- src/app.tsx
+++ src/app.tsx
@@ -1,5 +1,50 @@
 import React from "react";
+import styled from 'styled-components'
+import { ImageFileDragAndDropArea } from "./components/dnd";
+
+const Header = styled.header`
+  border-bottom: 1px solid silver;
+  line-height: 20px;
+  padding: 10px;
+  width: 100vw;
+  position: absolute;
+  top: 0;
+`
+
+const Title = styled.h1`
+  margin: 0;
+  padding: 0;
+  font-size: 20px;
+`
+
+const ImageArea = styled.div`
+  bottom: 41px;
+  position: absolute;
+  top: 41px;
+  width: 100vw;
+`
+
+const Footer = styled.footer`
+  border-top: 1px solid silver;
+  bottom: 0;
+  line-height: 24px;
+  padding: 8px;
+  width: 100vw;
+  position: absolute;
+`
 
 export const App = React.FC = () => {
-  return <h1>Image Editor!</h1>;
+  return (
+    <div>
+      <Header>
+        <Title>Image Editor</Title>
+      </Header>
+      <ImageArea>
+        <ImageFileDragAndDropArea onImageFileDrop={console.debug} />
+      </ImageArea>
+      <Footer>
+        Footer
+      </Footer>
+    </div>
+  );
 };
---
+++ src/components/dnd.tsx
@@ -0,0 +1,48 @@
+import React from "react";
+import styled from "styled-components";
+
+const DnD = styled.div`
+  align-items: center;
+  background-color: #ccf;
+  display: flex;
+  height: 100%;
+  justify-content: center;
+  width: 100%;
+`
+
+const Message = styled.div`
+  font-size: 16px;
+  font-weight: bold;
+`
+
+interface Props {
+  onImageFileDrop: (file: File) => void;
+}
+
+export const ImageFileDragAndDropArea: React.FC<Props> = (props) => {
+  const { onImageFileDrop } = props;
+
+  return (
+    <DnD
+      onDragOver={(event) => {
+        event.stopPropagation();
+        event.preventDefault();
+      }}
+      onDrop={(event) => {
+        console.debug(event)
+        event.preventDefault();
+        const file = event.dataTransfer.files.item(0);
+        if (file == null) {
+          return;
+        } else if (!file.type.startsWith("image/")) {
+          return;
+        }
+        onImageFileDrop(file);
+      }}
+    >
+      <Message>
+        Drop image file here!
+      </Message>
+    </DnD>
+  );
+};
