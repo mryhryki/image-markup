be5bef6cb817d3f2b880e2fcaf774dac9a2dfef6
mryhryki <mryhryki@gmail.com>
2022-02-02 16:03:42 +0900

    Change download filename

--- src/util/download_canvas_image.ts
+++ src/util/download_canvas_image.ts
@@ -2,6 +2,6 @@ export const downloadCanvasImage = (context: CanvasRenderingContext2D): void => 
   const dataUrl = context.canvas.toDataURL("image/png");
   const anchor = document.createElement("a");
   anchor.href = dataUrl;
-  anchor.download = "image.png";
+  anchor.download = `${new Date().toISOString().replace(/[^0-9]/g, "")}.png`;
   anchor.click();
 };
