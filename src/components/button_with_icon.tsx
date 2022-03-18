76051fb66776bab54a984273fef1a412823595d3
mryhryki <mryhryki@gmail.com>
2022-03-18 17:54:01 +0900

    Remove data URL and use image by URL

--- src/components/button_with_icon.tsx
+++ src/components/button_with_icon.tsx
@@ -1,17 +1,5 @@
 import React from "react";
 import styled from "styled-components";
-// @ts-ignore
-import ArrowIcon from "../icon/arrow.jpeg";
-// @ts-ignore
-import RectangleBorderIcon from "../icon/rectangle_border.jpeg";
-// @ts-ignore
-import DownloadIcon from "../icon/download.jpeg";
-// @ts-ignore
-import MaskIcon from "../icon/mask.jpeg";
-// @ts-ignore
-import TextIcon from "../icon/text.jpeg";
-// @ts-ignore
-import TrimIcon from "../icon/trim.jpeg";
 
 export const Button = styled.button`
   border-radius: 2px;
@@ -35,24 +23,6 @@ const Image = styled.img`
   height: 20px;
 `;
 
-const getImage = (iconName: string): string => {
-  switch (iconName) {
-    case "arrow":
-      return ArrowIcon;
-    case "rectangle_border":
-      return RectangleBorderIcon;
-    case "download":
-      return DownloadIcon;
-    case "mask":
-      return MaskIcon;
-    case "text":
-      return TextIcon;
-    case "trim":
-      return TrimIcon;
-  }
-  return "";
-};
-
 interface Props {
   alt: string;
   iconName: string;
@@ -65,7 +35,7 @@ export const ButtonWithIcon: React.FC<Props> = (props) => {
 
   return (
     <Button onClick={onClick} className={selected ? "selected" : ""}>
-      <Image alt={alt} src={getImage(iconName)} />
+      <Image alt={alt} src={`/icon/${iconName}.jpeg`} />
     </Button>
   );
 };
