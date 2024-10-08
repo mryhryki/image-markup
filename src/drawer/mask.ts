95913d98352850385776f59dd2fd1c3d4a5b70f6
mryhryki <mryhryki@gmail.com>
2024-10-08 17:54:10 +0900

    Update src/drawer/mask.ts

--- src/drawer/mask.ts
+++ src/drawer/mask.ts
@@ -1,7 +1,11 @@
 import { canvasRGB } from "stackblur-canvas";
-import { Position } from "../util/user_action_event";
+import type { Position } from "../util/user_action_event";
 
-export const drawMask = (context: CanvasRenderingContext2D, from: Position, to: Position): void => {
+export const drawMask = (
+  context: CanvasRenderingContext2D,
+  from: Position,
+  to: Position,
+): void => {
   const x = Math.min(from.x, to.x);
   const y = Math.min(from.y, to.y);
   const width = Math.abs(from.x - to.x);
